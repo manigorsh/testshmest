3ce764c691653ff71b66db1574ec50d545d9c704
Igor M <manigorsh@gmail.com>
2018-03-10 16:46:44 +0300

    translate

--- resources/lang/en/common.php
+++ resources/lang/en/common.php
@@ -4,4 +4,6 @@ return [
     'PUBLIC_OFFER' => 'Public Offer',
     'PROJECT_RULES' => 'Project Rules',
     'GAME_RULES' => 'Game Rules',
+    'SUPPORT' => 'Support'
+
 ];
--- resources/lang/ru/common.php
+++ resources/lang/ru/common.php
@@ -4,4 +4,5 @@ return [
     'PUBLIC_OFFER' => 'Публичная Офферта',
     'PROJECT_RULES' => 'Правила Проекта',
     'GAME_RULES' => 'Правила Игр',
+    'SUPPORT' => 'Техподдержка'
 ];
